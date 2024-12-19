68813e37a43da9498b6304631c4281fdcadd6886
brahpuriaM <pallavikumari536@gmail.com>
2024-12-19 12:00:05 +0530

    Create file

---
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(ListNode node : lists) {
+            while(node != null) {
+                q.add(node.val);
+                node = node.next;
+            }
+        }
+        ListNode ll = new ListNode(1);
+        ListNode merge = ll;
+        while(!q.isEmpty()) {
+            merge.next = new ListNode(q.remove());
+            merge = merge.next;
+        }
+        return ll.next;
+    }
+}
