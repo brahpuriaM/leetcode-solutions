606598a88a2d756896a636b5b688e065d78fa34a
brahpuriaM <pallavikumari536@gmail.com>
2025-02-02 00:36:50 +0530

    Create file

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) {
+            return 0;
+        }
+        int bestBuy = -1;
+        int maxProfit = 0;
+        for(int i = 0; i<prices.length; i++) {
+            if(bestBuy < 0 || prices[i] < bestBuy) {
+                bestBuy = prices[i];
+            }
+            if(bestBuy >= 0) {
+                int p = prices[i] - bestBuy;
+                if(p > maxProfit) {
+                    maxProfit = p;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
