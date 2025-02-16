256ca05647a7d3c4e3f8324d4c8932febb3de937
brahpuriaM <pallavikumari536@gmail.com>
2025-02-16 19:53:55 +0530

    File updated at 16/02/2025, 19:53:54

--- 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.java
+++ 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.java
@@ -21,10 +21,10 @@ class Solution {
         if(root.left == null && root.right == null) {
             return 1;
         }
-        if(root.left == null)
-            return minDepth(root.right) + 1;
         if(root.right == null)
             return minDepth(root.left) + 1;
+        if(root.left == null)
+            return minDepth(root.right) + 1;
 
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
