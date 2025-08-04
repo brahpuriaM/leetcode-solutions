b0e48463d1e01d8d5cf4c24db530e7f550523b2f
brahpuriaM <pallavikumari536@gmail.com>
2025-08-04 16:31:07 +0530

    Create 160-IntersectionOfTwoLinkedLists

---
+++ 160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,31 @@
+// Last updated: 04/08/2025, 16:31:07
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+
+        while(headA != null) {
+            if(!set.contains(headA.val)) {
+                set.add(headA);
+            }
+            headA = headA.next;
+        }
+        while(headB != null) {
+            if(set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
