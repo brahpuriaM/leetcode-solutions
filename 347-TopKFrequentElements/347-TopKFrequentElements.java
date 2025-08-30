13a84aa09c8ae66a67ba1d9a5b0da16aca60aee3
brahpuriaM <pallavikumari536@gmail.com>
2025-08-31 02:01:08 +0530

    File updated at 31/08/2025, 02:01:08

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,26 +1,29 @@
-// Last updated: 31/08/2025, 01:55:34
+// Last updated: 31/08/2025, 02:01:08
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] res = new int[k];
+        if(k== nums.length) {
+            return nums;
+        }
 
         Map<Integer, Integer> map = new HashMap<>();
-
         for(int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
-            (a,b) -> a.getValue() - b.getValue()
-        );
 
-        for(Map.Entry<Integer, Integer> entrySet : map.entrySet()) {
-            minHeap.offer(entrySet);
+        Queue<Integer> minHeap = new PriorityQueue<>(
+            (a,b) -> map.get(a) - map.get(b)
+        );
+        for(int num : map.keySet()) {
+            minHeap.add(num);
             if(minHeap.size() > k) {
                 minHeap.poll();
             }
         }
+
+        int[] res = new int[k];
         int j = 0;
         while (!minHeap.isEmpty()) {
-            res[j] = minHeap.poll().getKey();
+            res[j] = minHeap.poll();
             j++;
         }
         return res;
