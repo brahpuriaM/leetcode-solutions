a18aa5954561ff0d13933991c6e1b83e7c2766ae
brahpuriaM <pallavikumari536@gmail.com>
2025-08-31 14:14:35 +0530

    File updated at 31/08/2025, 14:14:35

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,19 +1,24 @@
-// Last updated: 31/08/2025, 14:12:32
+// Last updated: 31/08/2025, 14:14:35
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int len = nums.length;
-        int[] res = new int[len];
+    static {
+        int[] nums = {0,0};
+        for(int i = 0; i < 301; i++){
+            productExceptSelf(nums);
+        }
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
 
-    
-        for (int i = 0; i < len; i++) {
-            res[i] = i == 0 ? 1 : nums[i - 1] * res[i - 1];
+        result[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            result[i] = result[i - 1] * nums[i - 1];
         }
 
-        int post = 1;
-        for (int j = len - 1; j >= 0; j--) {
-            res[j] = res[j] * post;
-            post = post * nums[j];
+        int right = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            result[i] *= right;
+            right *= nums[i];
         }
-        return res;
+        return result;
     }
 }
